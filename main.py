4694527f32cccd035c86fb9fe7365d662db1d657
odin <odinmerlinmay@gmail.com>
2022-03-19 22:02:28 -0400

    Add some features for checking different price locations and accounting for blank price

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-from bs4 import BeautifulSoup
-import requests
-import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import csv
-from pprint import pprint
 
 browser = webdriver.Firefox()
 
@@ -33,30 +29,75 @@ def get_html(url=None):
     browser.implicitly_wait(2)
 
     # Grab title element
-    title = browser.find_element(by=By.CSS_SELECTOR, value="#productTitle")
+    try:
+        title = browser.find_element(by=By.CSS_SELECTOR, value="#productTitle")
+    except:
+        title = browser.find_element(by=By.CSS_SELECTOR, value="")
 
     # Grab price element
     # If theres an error (Like a missing element)...
     # If so, the element may be under a different css selector, so we try the other selector within the except block
     try:
         price = browser.find_element(by=By.CSS_SELECTOR, value="#price")
+        if "$" not in price.text:
+            raise Exception("Not the price")
     except:
-        # Grab price element
-        price = browser.find_element(by=By.CSS_SELECTOR, value=".a-text-price > span:nth-child(1)")
+        try:
+            # Grab price element
+            price = browser.find_element(by=By.CSS_SELECTOR, value=".a-text-price > span:nth-child(1)")
+            if "$" not in price.text:
+                raise Exception("Not the price")
+        except:
+            try:
+                # Grab price element
+                price = browser.find_element(by=By.CSS_SELECTOR, value=".a-text-price > span:nth-child(2)")
+                if "$" not in price.text:
+                    raise Exception("Not the price")
+            except:
+                try:
+                    price = browser.find_element(by=By.CSS_SELECTOR, value="#corePrice_feature_div > div:nth-child(1) > span:nth-child(1) > span:nth-child(1)")
+                    if "$" not in price.text:
+                        raise Exception("Not the price")
+                except:
+                    price = browser.find_element(by=By.CSS_SELECTOR, value=".offer-price")
 
     try:
         # Grab author element
         author = browser.find_element(by=By.CSS_SELECTOR, value=".contributorNameID")
     except:
-        author = browser.find_element(by=By.CSS_SELECTOR, value="span.author:nth-child(2) > a:nth-child(1)")
+        try:
+            author = browser.find_element(by=By.CSS_SELECTOR, value="span.author:nth-child(2) > a:nth-child(1)")
+        except:
+            try:
+                author = browser.find_element(by=By.CSS_SELECTOR, value=".author > a:nth-child(1)")
+            except:
+                author = ""
+
 
     # Grab publisher element
     publisher = browser.find_element(by=By.CSS_SELECTOR,
                                      value="ul.a-vertical:nth-child(1) > li:nth-child(1) > span:nth-child(1) > span:nth-child(2)")
 
     # Grab isbn element
-    isbn = browser.find_element(by=By.CSS_SELECTOR,
-                                value="ul.a-vertical:nth-child(1) > li:nth-child(4) > span:nth-child(1) > span:nth-child(2)")
+    try:
+        isbn = browser.find_element(by=By.CSS_SELECTOR,
+                                    value="ul.a-vertical:nth-child(1) > li:nth-child(4) > span:nth-child(1) > span:nth-child(2)")
+
+    except:
+        isbn = ""
+
+
+    print(title, author, publisher, isbn, price)
+    if author == "":
+        if isbn == "":
+            return title.text, author, publisher.text, isbn, price.text
+        else:
+            return title.text, author, publisher.text, isbn.text, price.text
+
+    if isbn == "":
+        return title.text, author.text, publisher.text, isbn, price.text
+
+
 
     return title.text, author.text, publisher.text, isbn.text, price.text
 
